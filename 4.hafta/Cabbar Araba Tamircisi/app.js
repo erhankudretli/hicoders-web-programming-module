54df4e172a567eedbe945e4c924ddc8737b21b1f
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-02 20:02:36 +0100

    deleted the variables

--- 4.hafta/Cabbar Araba Tamircisi/app.js
+++ 4.hafta/Cabbar Araba Tamircisi/app.js
@@ -4,36 +4,36 @@ let aracMarkasi;
 let aracRengi;
 
 if (hizmetZamani >= 5) {
-    console.log("Aracinizi kabul ediyoruz.")
+    console.log("Aracinizi kabul ediyoruz.");
 } else {
     if (aracRengi == "siyah" || aracRengi == "kirmizi") {
         if (aracMarkasi == "BMW") {
             if (aracRengi == "kirmizi" && (aracModeli >= 2010 && aracModeli <= 2020)) {
-                console.log("Aracinizi kabul ediyoruz.")
+                console.log("Aracinizi kabul ediyoruz.");
             } else if (aracModeli == 2020 && aracRengi == "siyah") {
-                console.log("Aracinizi kabul ediyoruz.")
+                console.log("Aracinizi kabul ediyoruz.");
             } else {
-                console.log("Aracinizi kabul ETMIYORUZ")
+                console.log("Aracinizi kabul ETMIYORUZ");
             }
 
         } else if (aracMarkasi == "Audi") {
             if ((aracModeli >= 2005 && aracModeli <= 2010) || (aracModeli >= 2014 && aracModeli <= 2020)) {
-                console.log("Aracinizi kabul ediyoruz.")
+                console.log("Aracinizi kabul ediyoruz.");
             } else {
-                console.log("Aracinizi kabul ETMIYORUZ")
+                console.log("Aracinizi kabul ETMIYORUZ");
             }
 
         } else if (aracMarkasi == "VW") {
             if (aracRengi == "siyah" && (aracModeli >= 2001 && aracModeli <= 2018)) {
-                console.log("Aracinizi kabul ediyoruz.")
+                console.log("Aracinizi kabul ediyoruz.");
             } else {
-                console.log("Aracinizi kabul ETMIYORUZ")
+                console.log("Aracinizi kabul ETMIYORUZ");
             }
 
         } else {
-            console.log("Aracinizi kabul ETMIYORUZ")
+            console.log("Aracinizi kabul ETMIYORUZ"); // renk siyah yada kirmizi degilse döngüsü
         }
     } else {
-        console.log("Aracinizi kabul ETMIYORUZ")
+        console.log("Aracinizi kabul ETMIYORUZ");  // hizmet zamani 5 yildan büyük degilse döngüsü
     }
 }
