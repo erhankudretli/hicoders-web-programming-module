6168abcd5f45970fdb131aed157248ed0de41182
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-02 19:25:00 +0100

    Added all web programming homeworks

---
+++ 4.hafta/WEP - Week 04 - Horoscope in Javascript/app.js
@@ -0,0 +1,88 @@
+const month = 3;
+const day = 12;
+
+if (month == 1) {
+    if (day <= 21) {
+        console.log("Burcunuz Oglak tir. ");
+    } else {
+        console.log("Burcunuz Kova dir.");
+    }
+}
+
+if (month == 2) {
+    if (day <= 19) {
+        console.log("Burcunuz Kova tir. ");
+    } else {
+        console.log("Burcunuz Balik dir.");
+    }
+}
+if (month == 3) {
+    if (day <= 20) {
+        console.log("Burcunuz Balik tir. ");
+    } else {
+        console.log("Burcunuz Koc dir.");
+    }
+}
+if (month == 4) {
+    if (day <= 20) {
+        console.log("Burcunuz Koc tir. ");
+    } else {
+        console.log("Burcunuz Boga dir.");
+    }
+}
+if (month == 5) {
+    if (day <= 21) {
+        console.log("Burcunuz Boga tir. ");
+    } else {
+        console.log("Burcunuz Ikizler dir.");
+    }
+}
+if (month == 6) {
+    if (day <= 21) {
+        console.log("Burcunuz Ikizler tir. ");
+    } else {
+        console.log("Burcunuz Yengec dir.");
+    }
+}
+if (month == 7) {
+    if (day <= 22) {
+        console.log("Burcunuz Yengec tir. ");
+    } else {
+        console.log("Burcunuz Aslan dir.");
+    }
+}
+if (month == 8) {
+    if (day <= 22) {
+        console.log("Burcunuz Aslan tir. ");
+    } else {
+        console.log("Burcunuz Basak dir.");
+    }
+}
+if (month == 9) {
+    if (day <= 22) {
+        console.log("Burcunuz Basak tir. ");
+    } else {
+        console.log("Burcunuz Terazi dir.");
+    }
+}
+if (month == 10) {
+    if (day <= 22) {
+        console.log("Burcunuz Terazi tir. ");
+    } else {
+        console.log("Burcunuz Akrep dir.");
+    }
+}
+if (month == 11) {
+    if (day <= 21) {
+        console.log("Burcunuz Akrep tir. ");
+    } else {
+        console.log("Burcunuz Yay dir.");
+    }
+}
+if (month == 12) {
+    if (day <= 21) {
+        console.log("Burcunuz Yay tir. ");
+    } else {
+        console.log("Burcunuz Oglak dir.");
+    }
+}
