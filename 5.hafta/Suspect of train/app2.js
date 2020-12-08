99c5840311a12a7f45891bb4e97b03dd31a2e33e
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-08 22:37:44 +0100

    added grades variable in the loop

--- 5.hafta/Suspect of train/app2.js
+++ 5.hafta/Suspect of train/app2.js
@@ -2,12 +2,15 @@ let kacHakkimVar = 4;
 let tahmin = 0;
 let hedefvagon = Math.floor(Math.random() * 10) + 1;
 let donmeSayisi=0;
+let puaniniz=0;
 
 while (kacHakkimVar != 0) {
     tahmin = parseInt(prompt("Sizce süpheli hangi vagondadir?"));
     donmeSayisi++;
     if (hedefvagon == tahmin) {
         alert("Tebrikler kazandiniz...." + donmeSayisi + ". seferde kazandiniz.");
+        puaniniz=100-(donmeSayisi-1)*25;
+        alert("Puaniniz: " + puaniniz);
         break;
     } // dis if 
     else if (hedefvagon > tahmin) {
