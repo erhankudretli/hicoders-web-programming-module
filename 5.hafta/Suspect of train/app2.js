d8f05fb1b4ea8b04a448b9e4e1287437f4d9ddbd
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-08 22:29:11 +0100

    added suspect of train homework inside of the 5. weeks files

---
+++ 5.hafta/Suspect of train/app2.js
@@ -0,0 +1,23 @@
+let kacHakkimVar = 4;
+let tahmin = 0;
+let hedefvagon = Math.floor(Math.random() * 10) + 1;
+let donmeSayisi=0;
+
+while (kacHakkimVar != 0) {
+    tahmin = parseInt(prompt("Sizce süpheli hangi vagondadir?"));
+    donmeSayisi++;
+    if (hedefvagon == tahmin) {
+        alert("Tebrikler kazandiniz...." + donmeSayisi + ". seferde kazandiniz.");
+        break;
+    } // dis if 
+    else if (hedefvagon > tahmin) {
+        alert("Süpheliyi daha arkada arayin...");
+    } else if (hedefvagon < tahmin) {
+        alert("Süpheliyi daha önde arayin...");
+    }
+    kacHakkimVar--;
+} // while kapama
+
+if (kacHakkimVar == 0) {
+    alert("Maalesef Kaybettiniz...");
+}
