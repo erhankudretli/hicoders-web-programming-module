cff74798f7f7e02b3e0449d2262a478395ffed54
Erhan Kudretli <erhankudretli@gmail.com>
2021-01-06 23:44:46 +0100

    added homework

---
+++ 8.week/Balik Ciftligi/app.js
@@ -0,0 +1,30 @@
+
+
+console.log(StokTakipEt(500));
+
+
+
+/*
+ Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
+ */
+
+console.log(fiyataGoreFiltrele(9,12));
+
+/*
+ Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
+ */
+
+
+console.log(sezonLokasyonFiltrele("BE","Winter"));
+
+
+
+sonKullanimTarihlerineBak();
+
+
+
+let abUlkeleri=["Norway","Spain","GREECE","Italy","France"];
+let butunUlkeler=fishFarm.map(ittem=> {
+    ittem.originCountry;
+})
+console.log(butunUlkeler)
